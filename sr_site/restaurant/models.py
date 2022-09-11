656def5707d5f22f3417154ec7bb6ebf6cd4b976
PYGAME <vdk45blg@gmail.co>
2022-09-11 14:42:33 +0900

    Initial commit

---
+++ sr_site/restaurant/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255, verbose_name='name')  # Text
+    description = models.TextField(blank=True, verbose_name='description')  # Description / Не обязательное
+    price = models.IntegerField(verbose_name='price')  # Price
+    photo = models.ImageField(upload_to='photos/%y/%m/%d', verbose_name='photo')  # Photo
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='updated')
+    is_published = models.BooleanField(default=True, verbose_name='published')  # Default = none
+
+
+
+
+
+
+
+
