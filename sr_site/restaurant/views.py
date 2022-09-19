6e1450fb327e2f5a84435b920dba624a412e5ad4
PYGAME <vdk45blg@gmail.co>
2022-09-19 13:32:49 +0900

    category.html

--- sr_site/restaurant/views.py
+++ sr_site/restaurant/views.py
@@ -6,12 +6,18 @@ from .models import *
 # Create your views here.
 def index(request):
     menu = Menu.objects.all()  #
+    categories = Category.objects.all()
     # menu = Menu.objects.order_by('-created_at')  # Sort by new if not sorty in Meta
     context = {
-        'title': 'All menu',  # tittle (index.html)
+        'title': 'Tat ca menu',  # tittle (index.html)
         'menu': menu,  # body (index.html)
+        'categories': categories
     }
     return render(request, template_name='restaurant/index.html', context=context)
 
 
-
+def get_category(request, category_id):
+    menu = Menu.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'restaurant/category.html', {'menu': menu, 'categories': categories, 'category': category, 'title': 'Menu'})
