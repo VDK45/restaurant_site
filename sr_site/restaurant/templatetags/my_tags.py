94fdd4c55ff712d3fdb761dc562ea74605190eb6
VDK <vdk45blg@gmail.com>
2022-10-21 14:09:19 +0900

    Show category if not empty (custom tag)

--- sr_site/restaurant/templatetags/my_tags.py
+++ sr_site/restaurant/templatetags/my_tags.py
@@ -5,14 +5,15 @@ from django.db.models import Count, F
 register = template.Library()
 
 
-@register.simple_tag(name='get_list_cat')  # Скобки необязтельно
+@register.simple_tag(name='get_list_cat')  # Скобки необязательно
 def get_categories():
     return Category.objects.all()
 
 
 @register.inclusion_tag('restaurant/list_categories.html')
 def show_categories(arg1, arg2):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('menu')).filter(cnt__gt=0)  # show category have 1 more menu
     return {"categories": categories, 'arg1': arg1, 'arg2': arg2}
 
 
